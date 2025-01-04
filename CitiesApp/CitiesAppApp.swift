bda4c1df6d0f64ae97ec9ebbeeb9d098f7f3f216
João Victor Vital <jvital@gmail.com>
2025-01-04 16:25:45 -0300

    Initial Commit

---
+++ CitiesApp/CitiesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesAppApp.swift
+//  CitiesApp
+//
+//  Created by jvic on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CitiesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
